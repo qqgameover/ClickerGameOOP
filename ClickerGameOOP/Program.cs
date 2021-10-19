37ad21982ef4f56b10bee7e9c7ff5b5cff7ac254
qqgameover <kasper35@live.no>
2021-10-19 17:23:52 +0200

    Initial commit

---
+++ ClickerGameOOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClickerGameOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
